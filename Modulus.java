3be91e44a64890374346318e0a884b7cc0064dd9
P.Priyanka <119730292+PriyankaPotti553@users.noreply.github.com>
2024-01-05 21:43:15 +0530

    import java.util.*; public class Module{     public static void main(String[] args){         Scanner n = new Scanner(System.in);         int a = n.nextInt();         int b = n.nextInt();         System.out.printf("%d",a%b);     } }

---
+++ Modulus.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Module{
+    public static void main(String[] args){
+        Scanner n = new Scanner(System.in);
+        int a = n.nextInt();
+        int b = n.nextInt();
+        System.out.printf("%d",a%b);
+    }
+}
