1be26f856688a9c9176d193969abae54a6b684ac
P.Priyanka <119730292+PriyankaPotti553@users.noreply.github.com>
2024-01-05 21:46:47 +0530

    import java.util.*; public class Div{     public static void main(String[] args){         Scanner k = new Scanner(System.in);         int a = k.nextInt();         int b = k.nextInt();         System.out.printf("%d",a/b);     } }

---
+++ Division.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Div{
+    public static void main(String[] args){
+        Scanner k = new Scanner(System.in);
+        int a = k.nextInt();
+        int b = k.nextInt();
+        System.out.printf("%d",a/b);
+    }
+}
