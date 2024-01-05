58d7e03f3c1673299d1afcd5071a201068db4508
P.Priyanka <119730292+PriyankaPotti553@users.noreply.github.com>
2024-01-05 21:39:25 +0530

    import java.util.*; public class Str{     public static void main(String[] args){         Scanner p = new Scanner(System.in);         String s = p.nextLine();         System.out.printf(s);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Str{
+    public static void main(String[] args){
+        Scanner p = new Scanner(System.in);
+        String s = p.nextLine();
+        System.out.printf(s);
+    }
+}
