1a504dbd4061053c73fc0102872ee3c61a456a87
P.Priyanka <119730292+PriyankaPotti553@users.noreply.github.com>
2024-01-07 12:03:39 +0530

    import java.util.*; public class Noodles{     public static void main(String[] args){         Scanner l = new Scanner(System.in);         int x = l.nextInt();         int y = l.nextInt();         System.out.printf("%d",x*y);     } }

---
+++ Instant_Noodles.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Noodles{
+    public static void main(String[] args){
+        Scanner l = new Scanner(System.in);
+        int x = l.nextInt();
+        int y = l.nextInt();
+        System.out.printf("%d",x*y);
+    }
+}
