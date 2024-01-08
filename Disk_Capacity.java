6cb2a15b57b3d9ae323979c3bd9a38c6e1494422
P.Priyanka <119730292+PriyankaPotti553@users.noreply.github.com>
2024-01-08 12:00:56 +0530

    import java.util.*; public class Disk{     public static void main(String[] args){         Scanner s = new Scanner(System.in);         int p = s.nextInt();         int q = s.nextInt();         int r = s.nextInt();         System.out.println(2*p*q*r*512);
        }
    }

---
+++ Disk_Capacity.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Disk{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int p = s.nextInt();
+        int q = s.nextInt();
+        int r = s.nextInt();
+        System.out.println(2*p*q*r*512);
+        
+        
+    }
+}
