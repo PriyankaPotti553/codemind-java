e35340949d0c6b464dd2152a23b4483e455591b7
P.Priyanka <119730292+PriyankaPotti553@users.noreply.github.com>
2024-01-08 11:05:40 +0530

    import java.util.Scanner; public class Frames{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         int x = sc.nextInt();         int y = sc.nextInt();         int z = sc.nextInt();         System.out.println((2*(x+y))*z);
        }
    }

---
+++ Frames.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Frames{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int z = sc.nextInt();
+        System.out.println((2*(x+y))*z);
+        
+    }
+}
