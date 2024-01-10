a43d1a6c746ed0aafe2064bc7b184bbef814f8eb
P.Priyanka <119730292+PriyankaPotti553@users.noreply.github.com>
2024-01-10 18:29:41 +0530

    import java.util.*; public class Bridge{     public static void main(String[] args){         Scanner p = new Scanner(System.in);         int x = p.nextInt();         int y = p.nextInt();         int z = p.nextInt();         System.out.println((z-y)/x);
        }
    }

--- Can_Cross_the_Bridge.java
+++ Can_Cross_the_Bridge.java
@@ -1,10 +1,11 @@
 import java.util.*;
 public class Bridge{
     public static void main(String[] args){
-        Scanner n = new Scanner(System.in);
-        int x = n.nextInt();
-        int y = n.nextInt();
-        int z = n.nextInt();
+        Scanner p = new Scanner(System.in);
+        int x = p.nextInt();
+        int y = p.nextInt();
+        int z = p.nextInt();
         System.out.println((z-y)/x);
+        
     }
 }
