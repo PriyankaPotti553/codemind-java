f5e946d023037655ca93a3ffc0515a0cdd3501b2
P.Priyanka <119730292+PriyankaPotti553@users.noreply.github.com>
2024-01-12 22:19:27 +0530

    import java.util.*; public class c{     public static void main(String[] args){         Scanner p = new Scanner(System.in);         int a = p.nextInt();         int b = p.nextInt();         int c = p.nextInt();         System.out.println(a*b*c+" "+"KB");     } }

---
+++ Capacity.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class c{
+    public static void main(String[] args){
+        Scanner p = new Scanner(System.in);
+        int a = p.nextInt();
+        int b = p.nextInt();
+        int c = p.nextInt();
+        System.out.println(a*b*c+" "+"KB");
+    }
+}
