516a815a40e572eb7deeeaf5ad02e4adc6cc006b
P.Priyanka <119730292+PriyankaPotti553@users.noreply.github.com>
2024-01-12 22:27:42 +0530

    import java.util.*; public class v{     public static void main(String[] args){         Scanner p = new Scanner(System.in);         int a = p.nextInt();         int b = p.nextInt();         System.out.println(b/a);     } }

---
+++ Required_Points:.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class v{
+    public static void main(String[] args){
+        Scanner p = new Scanner(System.in);
+        int a = p.nextInt();
+        int b = p.nextInt();
+        System.out.println(b/a);
+    }
+}
