30e1c064dbc22a103e261d14579c1981c8832212
P.Priyanka <119730292+PriyankaPotti553@users.noreply.github.com>
2024-01-16 18:13:30 +0530

    import java.util.*; public class Kings{     public static void main(String[] args){         Scanner p = new Scanner(System.in);         int x = p.nextInt();         int y = p.nextInt();         System.out.println(x*5+y*7);     } }

---
+++ King_Tours_.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Kings{
+    public static void main(String[] args){
+        Scanner p = new Scanner(System.in);
+        int x = p.nextInt();
+        int y = p.nextInt();
+        System.out.println(x*5+y*7);
+    }
+}
