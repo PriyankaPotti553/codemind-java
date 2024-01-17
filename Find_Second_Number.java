69ef85eb5038374a212bef9bc76dc200c5bb433f
P.Priyanka <119730292+PriyankaPotti553@users.noreply.github.com>
2024-01-17 14:59:39 +0530

    import java.util.*; public class Second{     public static void main(String[] args){         Scanner p = new Scanner(System.in);         int x = p.nextInt();         int y = p.nextInt();         System.out.println(2*x-y);     } }

---
+++ Find_Second_Number.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Second{
+    public static void main(String[] args){
+        Scanner p = new Scanner(System.in);
+        int x = p.nextInt();
+        int y = p.nextInt();
+        System.out.println(2*x-y);
+    }
+}
