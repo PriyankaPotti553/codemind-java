bc6ef61d568b8eede5c7a79c42ecdec448576c7d
P.Priyanka <119730292+PriyankaPotti553@users.noreply.github.com>
2024-01-19 21:18:09 +0530

    import java.util.*; public class Taps{     public static void main(String[] args){         Scanner p = new Scanner(System.in);         int x = p.nextInt();         int y = p.nextInt();         System.out.println((x*y)/(x+y));     } }

--- Water_Taps.java
+++ Water_Taps.java
@@ -1,23 +1,9 @@
-import java.util.Scanner;
-
-public class TankFillTime {
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        int timeA = scanner.nextInt();
-        int timeB = scanner.nextInt();
-        int fillTime = calculateFillTime(timeA, timeB);
-        System.out.println(fillTime/2);
-        scanner.close();
-    }
-
-    
-    public static int calculateFillTime(int timeA, int timeB) {
-        double harmonicMean = 2.0 / ((1.0 / timeA) + (1.0 / timeB));
-        int fillTime = (int) Math.ceil(harmonicMean);
-
-        return fillTime;
-        
+import java.util.*;
+public class Taps{
+    public static void main(String[] args){
+        Scanner p = new Scanner(System.in);
+        int x = p.nextInt();
+        int y = p.nextInt();
+        System.out.println((x*y)/(x+y));
     }
 }
