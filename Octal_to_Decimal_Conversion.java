6f59baa5bb3de26395baa3b0c9ffe7e72a5867bd
P.Priyanka <119730292+PriyankaPotti553@users.noreply.github.com>
2024-01-19 21:30:03 +0530

    import java.util.*; public class Oct{     public static void main(String[] args){         Scanner p = new Scanner(System.in);         String n = p.next();         int decimal = Integer.parseInt(n,8);         System.out.println(decimal);     } }

---
+++ Octal_to_Decimal_Conversion.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Oct{
+    public static void main(String[] args){
+        Scanner p = new Scanner(System.in);
+        String n = p.next();
+        int decimal = Integer.parseInt(n,8);
+        System.out.println(decimal);
+    }
+}
