c14921273713593628d919e5b74dcdb01c7a14ee
P.Priyanka <119730292+PriyankaPotti553@users.noreply.github.com>
2024-01-26 16:53:46 +0530

    import java.util.*; public class Juliet{     public static void main(String[] args){         Scanner p = new Scanner(System.in);         int x = p.nextInt();         int y = p.nextInt();         int z = p.nextInt();         System.out.println(((x*5)+(y*10))/z);     } }

---
+++ Romeo_and_Juliet.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Juliet{
+    public static void main(String[] args){
+        Scanner p = new Scanner(System.in);
+        int x = p.nextInt();
+        int y = p.nextInt();
+        int z = p.nextInt();
+        System.out.println(((x*5)+(y*10))/z);
+    }
+}
