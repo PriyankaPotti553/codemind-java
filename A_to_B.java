622e7d0d8a1cfbddd29e9f84aecf000c5e1e1a2a
P.Priyanka <119730292+PriyankaPotti553@users.noreply.github.com>
2024-01-29 21:05:11 +0530

    import java.util.*; public class Alphabets{     public static void main(String[] args){         Scanner s = new Scanner(System.in);         int a = s.nextInt();         int b = s.nextInt();         for(int i = a;i <= b;i++){             System.out.printf("%d ",i);         }     } }

---
+++ A_to_B.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Alphabets{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        for(int i = a;i <= b;i++){
+            System.out.printf("%d ",i);
+        }
+    }
+}
