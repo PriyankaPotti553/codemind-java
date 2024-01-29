ed50dbf3682e79bfc5ac27605d5e29a9eaaae151
P.Priyanka <119730292+PriyankaPotti553@users.noreply.github.com>
2024-01-29 21:01:27 +0530

    import java.util.*; public class Loop{     public static void main(String[] args){         Scanner p = new Scanner(System.in);         int n = p.nextInt();         for(int i=n;i>=1;i--){             System.out.printf("%d ",i);         }         //System.out.printf("%d",i);     } }

---
+++ N_to_1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Loop{
+    public static void main(String[] args){
+        Scanner p = new Scanner(System.in);
+        int n = p.nextInt();
+        for(int i=n;i>=1;i--){
+            System.out.printf("%d ",i);
+        }
+        //System.out.printf("%d",i);
+    }
+}
