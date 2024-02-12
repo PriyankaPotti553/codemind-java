a317e8a2af176f98d80fb78a76cdc53316dd9e65
P.Priyanka <119730292+PriyankaPotti553@users.noreply.github.com>
2024-02-12 12:16:39 +0530

    import java.util.*; public class Pattern6{     public static void main(String[] args){         Scanner s = new Scanner(System.in);         int n = s.nextInt();         for(int i = n;i >= 1;i--){             for(int j = n;j >= 1;j--){                 System.out.printf("%d ",j);             }             System.out.printf(" ");         }     } }

---
+++ print_the_following_pattern_6.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Pattern6{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        for(int i = n;i >= 1;i--){
+            for(int j = n;j >= 1;j--){
+                System.out.printf("%d ",j);
+            }
+            System.out.printf("
+");
+        }
+    }
+}
