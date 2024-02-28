880b6beb6c7214da7f3531fb004c7ba91e2ad839
P.Priyanka <119730292+PriyankaPotti553@users.noreply.github.com>
2024-02-28 11:25:07 +0530

    import java.util.*; public class Monk{     public static void main(String[] args){         Scanner s = new Scanner(System.in);         int n = s.nextInt();         int[] a = new int[n];         int[] b = new int[n];         for(int i = 0;i < n;i++){             a[i] = s.nextInt();         }         for(int i = 0;i < n;i++){             b[i] = s.nextInt();         }         int[] c = new int[n];         for(int i = 0;i < n;i++){             c[i] = a[i] + b[i];             System.out.print(c[i]+" ");         }     } }

---
+++ Monk_and_Welcome_Problem.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Monk{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int[] a = new int[n];
+        int[] b = new int[n];
+        for(int i = 0;i < n;i++){
+            a[i] = s.nextInt();
+        }
+        for(int i = 0;i < n;i++){
+            b[i] = s.nextInt();
+        }
+        int[] c = new int[n];
+        for(int i = 0;i < n;i++){
+            c[i] = a[i] + b[i];
+            System.out.print(c[i]+" ");
+        }
+    }
+}
