efc291e6425b8e35e95bbd5f41dd86471d5ecb22
P.Priyanka <119730292+PriyankaPotti553@users.noreply.github.com>
2024-03-19 11:26:27 +0530

    import java.util.*; public class Avarage{     public static void main(String[] args){         Scanner s = new Scanner(System.in);         int n = s.nextInt();         int sum = 0;         int a;         int count = 0;         int[] arr = new int[n];         for(int i = 0;i <= n-1;i++){             arr[i] = s.nextInt();         }         for(int i = 0;i <= n-1;i++){            sum += arr[i];         }         a = sum/n;         for(int i =0;i<=n-1;i++){             if(arr[i] <= a){                 count++;             }         }         System.out.println(count);     } }

---
+++ Elements_smaller_than_average.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Avarage{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int sum = 0;
+        int a;
+        int count = 0;
+        int[] arr = new int[n];
+        for(int i = 0;i <= n-1;i++){
+            arr[i] = s.nextInt();
+        }
+        for(int i = 0;i <= n-1;i++){
+           sum += arr[i]; 
+        }
+        a = sum/n;
+        for(int i =0;i<=n-1;i++){
+            if(arr[i] <= a){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
